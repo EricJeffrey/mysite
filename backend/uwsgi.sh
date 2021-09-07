4a7a9af2ca024a17d0021b394bd36bd27fcb1e04
EricJeffrey <1719937412@qq.com>
2021-09-07 12:48:57 +0800

    fix cmdline param

--- backend/uwsgi.sh
+++ backend/uwsgi.sh
@@ -1,3 +1,3 @@
 # -d daemon
 # app(python file app.py):app(application name)
-uwsgi -d 1 --http 0.0.0.0:8000 --module app:app
+uwsgi -d /data/site-of-jeff/uwsgi.log --http 0.0.0.0:8000 --module app:app
